22c66d33350007b5b2a3c7468bd6bd07f07711ae
Ransika <57299043+RansikaP@users.noreply.github.com>
2024-03-13 19:12:03 -0400

    pass 21th stage

--- src/main/java/Server/Replica.java
+++ src/main/java/Server/Replica.java
@@ -74,10 +74,8 @@ public class Replica extends Server{
             char[] buffer = new char[fileSize];
             int bytesRead = reader.read(buffer, 0, fileSize - 1);
             String rdbFile = new String(buffer, 0, fileSize);
-            while(!reader.ready()) {
-
-            }
-            System.out.println(reader.readLine());
+            if (reader.ready())
+                System.out.println(reader.readLine());
             ExecutorService executor = Executors.newSingleThreadExecutor();
             executor.submit(new ReplicaHandler(masterSocket, this, this.cache));
             executor.shutdown();

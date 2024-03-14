3f1fa9178d62f2b3ae0a26069029210e3ceebdd3
Ransika <57299043+RansikaP@users.noreply.github.com>
2024-03-13 23:12:19 -0400

    pass 21th stage

--- src/main/java/Handler/ReplicaHandler.java
+++ src/main/java/Handler/ReplicaHandler.java
@@ -78,8 +78,9 @@ public class ReplicaHandler extends ClientHandler implements Runnable{
             System.out.println(out);
             System.out.println(this.getClientSocket());
             this.getClientSocket().close();
+            this.getClientSocket().getOutputStream().
             Socket test = new Socket(this.server.getMasterHost(), this.server.getMasterPort());
-            out = "*3\\r\\n$8\\r\\nREPLCONF\\r\\n$3\\r\\nACK\\r\\n$1\\r\\n0\\r\\n";
+            out = "*3\r\n$8\r\nREPLCONF\r\n$3\r\nACK\r\n$1\r\n0\r\n";
             test.getOutputStream().write(out.getBytes());
             test.getOutputStream().flush();
         }

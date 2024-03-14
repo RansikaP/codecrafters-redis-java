e72f5942e628e9544096f2310e125203acbf14a5
Ransika <57299043+RansikaP@users.noreply.github.com>
2024-03-13 20:01:24 -0400

    pass 21th stage

--- src/main/java/Server/Replica.java
+++ src/main/java/Server/Replica.java
@@ -74,12 +74,13 @@ public class Replica extends Server{
             int bytesRead = reader.read(buffer, 0, fileSize - 1);
             String rdbFile = new String(buffer, 0, fileSize);
             System.out.println(reader.ready());
+            System.out.println("Souck: " + masterSocket.toString() + "\n running on this thread: " + Thread.activeCount());
             if (reader.ready()) {
                 System.out.println("reader is ready: " + reader.readLine());
-                ExecutorService executor = Executors.newSingleThreadExecutor();
-                executor.submit(new ReplicaHandler(masterSocket, this, this.cache));
-                executor.shutdown();
             }
+            ExecutorService executor = Executors.newSingleThreadExecutor();
+            executor.submit(new ReplicaHandler(masterSocket, this, this.cache));
+            executor.shutdown();
         }
     }
 }
